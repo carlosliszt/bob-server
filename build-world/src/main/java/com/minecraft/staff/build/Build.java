f0b282a8463ea96082fe2729cc53e1a96ea9829d
carlo <124646177+skipdevelopment@users.noreply.github.com>
2024-11-22 22:02:22 -0300

    better build server system

--- build-world/src/main/java/com/minecraft/staff/build/Build.java
+++ build-world/src/main/java/com/minecraft/staff/build/Build.java
@@ -1,15 +1,32 @@
 package com.minecraft.staff.build;
 
+import com.minecraft.core.Constants;
+import com.minecraft.core.bukkit.BukkitGame;
+import com.minecraft.core.bukkit.server.BukkitServerStorage;
+import com.minecraft.core.server.ServerType;
+import com.minecraft.staff.build.command.CreateWorld;
 import com.minecraft.staff.build.listener.PlayerListener;
 import org.bukkit.Bukkit;
 import org.bukkit.WorldCreator;
 import org.bukkit.WorldType;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class Build extends JavaPlugin {
+public class Build extends BukkitGame {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        saveDefaultConfig();
+    }
 
     @Override
     public void onEnable() {
+        super.onEnable();
+
+        Constants.setServerType(ServerType.UNKNOWN);
+        Constants.setLobbyType(ServerType.MAIN_LOBBY);
+
+        Bukkit.unloadWorld("world", false);
+
         if (Bukkit.getServer().getWorld("build") == null) {
             System.out.println("Creating build world...");
             WorldCreator wc = new WorldCreator("build");
@@ -20,7 +37,17 @@ public class Build extends JavaPlugin {
             System.out.println("Build world already exists.");
         }
 
+        startServerDump();
+
+        getEngine().getBukkitFrame().registerCommands(new CreateWorld());
+
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        ((BukkitServerStorage) getServerStorage()).subscribeProxyCount();
+
     }
 
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
